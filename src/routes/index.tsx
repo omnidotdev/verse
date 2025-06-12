7f73fbd5a70495cb2bf44a0ae8fbe74e7901dc77
Brian Cooper <brian@brian-cooper.com>
2025-06-12 11:20:35 -0500

    chore: use updated Garden package

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { Garden, type GardenTypes } from "@omnidev/garden";
+import { Garden, type GardenTypes } from "@omnidotdev/garden";
 // import {
 //   ReactFlow,
 //   Background,
