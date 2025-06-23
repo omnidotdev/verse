fa5ea9e7f41182dd85ad0b2c6c225085f25e1a53
Brian Cooper <brian@brian-cooper.com>
2025-06-23 09:17:05 -0500

    build(vite): add dev HTTPS/TLS support

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,26 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsConfigPaths from 'vite-tsconfig-paths'
+import tsConfigPaths from "vite-tsconfig-paths";
 import tanstackRouter from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
 const viteConfig = defineConfig({
-	// TODO HTTPS
-	// https://github.com/vitejs/vite-plugin-basic-ssl?tab=readme-ov-file
-	server: {
-		allowedHosts: ["verse.omni.dev"],
-	},
-	plugins: [
-		tailwindcss(),
-		tsConfigPaths({
-      projects: ['./tsconfig.json'],
+  server: {
+    allowedHosts: ["verse.omni.dev"],
+  },
+  plugins: [
+    mkcert(),
+    tailwindcss(),
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
     }),
-		tanstackRouter({
-					target: "react",
-					autoCodeSplitting: true,
-				}),
-				react(),
-	],
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
 });
 
 export default viteConfig;
